40a308a0f67e8d8a24699456773198a69d21110e
GreenShark777 <greenshark1740@gmail.com>
2021-10-03 21:06:09 +0200

    Creata eccezione per proiettili che vanno oltre i solidi

--- TeamWartortleGame/Assets/Scripts/Others/BulletsBehaviour.cs
+++ TeamWartortleGame/Assets/Scripts/Others/BulletsBehaviour.cs
@@ -10,25 +10,28 @@ public class BulletsBehaviour : MonoBehaviour
     private ParticleSystem shotPS = default, //Effetto di sparo
         hitPS = default; //Effetto di oggetto colpito
 
+    //indica se questo proiettile può andare oltre gli oggetti fisici o meno
+    [SerializeField]
+    private bool goesThroughSolids = false;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //se si collide con un oggetto fisico...
-        if (!collision.isTrigger) { BulletHitSolid(); }
+        //se non si possono oltepassare oggetti solidi e si collide con un oggetto fisico, il proiettile viene disattivato
+        if (!goesThroughSolids && !collision.isTrigger) { BulletHitSolid(); }
 
     }
 
     private void OnEnable()
     {
-        //se esiste il riferimento al particellare di oggetto colpito...
+        //se esiste il riferimento al particellare di sparo...
         if (shotPS)
         {
-            //...lo fa partire...
+            //...lo fa partire
             shotPS.Play();
-            //...se non lo è già, lo sparenta al proiettile
-            //if (shotPS.transform.parent) hitPS.transform.parent = null;
 
         }
+        else { Debug.LogError("Al proiettile " + gameObject.name + " manca il riferimento al particellare di sparo"); }
 
     }
 
@@ -45,6 +48,7 @@ public class BulletsBehaviour : MonoBehaviour
             hitPS.transform.position = transform.position;
 
         }
+        else { Debug.LogError("Al proiettile " + gameObject.name + " manca il riferimento al particellare di oggetto colpito"); }
         //disattiva questo proiettile
         gameObject.SetActive(false);
 
