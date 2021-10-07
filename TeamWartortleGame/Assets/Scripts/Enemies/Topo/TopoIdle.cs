09e6c6ef6b132b93a7cee40c58b8bfe96e69faa4
Pytodoom <kevincicero98@gmail.com>
2021-10-07 23:06:33 +0200

    sparo topo

--- TeamWartortleGame/Assets/Scripts/Enemies/Topo/TopoIdle.cs
+++ TeamWartortleGame/Assets/Scripts/Enemies/Topo/TopoIdle.cs
@@ -12,23 +12,31 @@ public class TopoIdle : TopoAbstract
     //Script manager da cui chiamare i suoi metodi
     private TopoManagerSTM topoManager;
 
+    //Game object che controllerà quale tra orizzontale e verticale è attivo e decidera di conseguenza la direzione dei proiettili
+    //In direzione verticale spara in basso e in alto e a destra e sinistra in direzione orizzontale
+    private bool horizontal;
+
+    private Transform shootPos;
+
     public override void StateEnter() {
         //Inizializzo lo startTime al tempo corrente
         startTime = shootStartTimer = Time.time;
         //Prendo lo script manager
         topoManager = GetComponent<TopoManagerSTM>();
+        //Il gameobject orizzontale è attivo, se si lo prendo come current
+        if (transform.GetChild(0).GetChild(0).gameObject.activeSelf)
+            horizontal = true;
+        else horizontal = false;
+
+        // prima ottengo il figlio "Parent" dopo di che ottengo il suo ultimo figlio, "Fiamma ratto"
+        shootPos = transform.GetChild(0).GetChild(transform.GetChild(0).transform.childCount - 1);
+
     }
 
     public override void StateUpdate() {
-        //Quando il tempo corrente azzerato è maggiore del tempo da raggiungere
-        if (Time.time - shootStartTimer > timerToReach / 2)
-        {
-            projectiles[0] = ObjectPooling.inst.SpawnFromPool("Fiamma", transform.position, Quaternion.identity);
-            projectiles[1] = ObjectPooling.inst.SpawnFromPool("Fiamma", transform.position, Quaternion.identity);
-            projectiles[0].GetComponent<Rigidbody2D>().AddForce(Vector2.left * 5f, ForceMode2D.Impulse);
-            projectiles[1].GetComponent<Rigidbody2D>().AddForce(Vector2.right * 5f, ForceMode2D.Impulse);
-            shootStartTimer = 9999;
-        }
+
+        Shoot();
+
         if (Time.time - startTime > timerToReach)
         {
             //Passo allo stato movement del topo
@@ -49,4 +57,29 @@ public class TopoIdle : TopoAbstract
     public override void StateCollisionExit(Collision2D collision) { }
 
     public override void StateExit() { }
+
+    private void Shoot()
+    {
+        //Quando il tempo corrente azzerato è maggiore del tempo da raggiungere
+        if (Time.time - shootStartTimer > timerToReach / 2)
+        {
+            //Instanzio i proiettili dall'object pooling
+            projectiles[0] = ObjectPooling.inst.SpawnFromPool("Fiamma", shootPos.position, Quaternion.identity);
+            projectiles[1] = ObjectPooling.inst.SpawnFromPool("Fiamma", shootPos.position, Quaternion.identity);
+            //Se sono in orizzontale sparo a destra e a sinistra
+            if (horizontal)
+            {
+                projectiles[0].GetComponent<Rigidbody2D>().AddForce(Vector2.left * 5f, ForceMode2D.Impulse);
+                projectiles[1].GetComponent<Rigidbody2D>().AddForce(Vector2.right * 5f, ForceMode2D.Impulse);
+            }
+            //Altrimenti sparo sopra e in basso
+            else
+            {
+                projectiles[0].GetComponent<Rigidbody2D>().AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+                projectiles[1].GetComponent<Rigidbody2D>().AddForce(Vector2.down * 5f, ForceMode2D.Impulse);
+            }
+            //Imposto il timer ad un valore irraggiungibile
+            shootStartTimer = 9999;
+        }
+    }
 }
---
+++ TeamWartortleGame/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //Nome del tag del target da colpire
+    [SerializeField]
+    private string target;
+
+    //Danno
+    [SerializeField]
+    private float dmg = 1;
+
+    [SerializeField]
+    private float timeToDestroy = 3;
+
+    private void OnEnable()
+    {
+        Invoke("ReAddToPool",timeToDestroy);
+    }
+
+    private void ReAddToPool()
+    {
+        if (gameObject.activeSelf)
+            ObjectPooling.inst.ReAddObjectToPool("Fiamma", this.gameObject);
+    }
+
+    //Con trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Se collido con il target
+        if (collision.CompareTag(target))
+        {
+            //Chiamo il suo Damage
+            IDamageable temp = collision.transform.parent.GetComponent<IDamageable>();
+            if (temp != null)
+                temp.Damage(dmg);
+            ReAddToPool();
+        }
+    }
+
+    //ma anche con collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Se collido con il target
+        if (collision.gameObject.CompareTag(target))
+        {
+            //Chiamo il suo Damage
+            IDamageable temp = collision.transform.parent.GetComponent<IDamageable>();
+            if (temp != null)
+                temp.Damage(dmg);
+        }
+
+        ReAddToPool();
+    }
+}
