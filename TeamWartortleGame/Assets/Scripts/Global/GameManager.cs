cf27f23110386ceae8c591ae68d7359e2e7865c4
Pytodoom <kevincicero98@gmail.com>
2021-10-11 15:28:37 +0200

    esecuzione e purificazione

--- TeamWartortleGame/Assets/Scripts/Global/GameManager.cs
+++ TeamWartortleGame/Assets/Scripts/Global/GameManager.cs
@@ -16,8 +16,12 @@ public class GameManager : MonoBehaviour
     private GameObject player;
 
     //Nemico corrente per la decisione(esecuzione o purificazione)
+    [HideInInspector]
     public GameObject currentEnemy = null;
 
+    //GameObject della purificazione(spada) e esecuzione(croce)
+    [SerializeField]
+    private GameObject spada, croce;
     private void Start()
     {
         //Singleton pattern
@@ -43,7 +47,6 @@ public class GameManager : MonoBehaviour
     {
         //Riferimento all'animator del player
         Animator anPlayer = player.GetComponent<Animator>();
-        anPlayer.SetTrigger("Execution");
         //Ottengo tutti i suoi sprite renderer per colorarli di nero
         SpriteRenderer[] enemySprites = enemy.GetComponentsInChildren<SpriteRenderer>(true);
         //Timer per il cambio dei colori
@@ -53,6 +56,11 @@ public class GameManager : MonoBehaviour
         //Se ho scelto purificazione
         if (value == 0)
         {
+            anPlayer.SetTrigger("Purify");
+            //Attivo la spada
+            spada.SetActive(true);
+            //La posiziono sopra il nemico
+            spada.transform.parent.position = enemy.transform.position + Vector3.up * 3;
             while (timer < 1)
             {      
                 //Aumento il timer, il tutto verrà effettuato in un secondo
@@ -75,11 +83,38 @@ public class GameManager : MonoBehaviour
             enHealth.currentColor = currentColor;
             //Riazzero il timer per poterlo riutilizzare
             timer = 0;
-
+   
         }
         //Altrimenti se ho scelto esecuzione
         else 
         {
+            anPlayer.SetTrigger("Execution");
+            //Attivo la spada
+            croce.SetActive(true);
+            //La posiziono sopra il nemico
+            croce.transform.parent.position = enemy.transform.position + Vector3.up * 3;
+            while (timer < 1)
+            {
+                //Aumento il timer, il tutto verrà effettuato in un secondo
+                timer += Time.deltaTime / 1f;
+                //Assegno il colore a quello corrente
+                currentColor = Color.Lerp(enHealth.startColor, Color.red, timer);
+                //e assegno il colore raggiunto allo sprite corrente del nemico
+
+                //Per ogni sprite del nemico
+                for (int i = 0; i < enemySprites.Length; i++)
+                {
+                    //Assegnoi il colore raggiunto
+                    enemySprites[i].color = currentColor;
+
+                }
+                yield return null;
+            }
+
+            //Assegno il colore corrente al nemico
+            enHealth.currentColor = currentColor;
+            //Riazzero il timer per poterlo riutilizzare
+            timer = 0;
 
         }
 
@@ -109,7 +144,10 @@ public class GameManager : MonoBehaviour
             }
             yield return null;
         }
-       
+        //Disattivo la spada e la croce se attivi
+        if (spada.activeSelf) spada.SetActive(false);
+        if (croce.activeSelf) croce.SetActive(false);
+        //Disattivo il nemico
         enemy.SetActive(false);
         yield return null;
     }
