91bb3222c1e8e041f5ddb2170b4bb58d470c1b59
GreenShark777 <greenshark1740@gmail.com>
2021-10-18 15:48:34 +0200

    Cambiamenti fatti da Kevin spostati qui

--- TeamWartortleGame/Assets/Scripts/Others/CubicSlimeBehaviour.cs
+++ TeamWartortleGame/Assets/Scripts/Others/CubicSlimeBehaviour.cs
@@ -17,7 +17,7 @@ public class CubicSlimeBehaviour : MonoBehaviour
         speed = 20, 
         attackSpeed = 0.2f;
     
-    private float startSpeed = 0;
+    //private float startSpeed = 0;
 
     private bool followPlayer = true;
 
@@ -33,9 +33,11 @@ public class CubicSlimeBehaviour : MonoBehaviour
         spriteSheet = new GameObject[spritesContainer.childCount];
         //cicla ogni figlio nel contenitore degli sprite e li aggiunge alla lista
         foreach (Transform child in spritesContainer) { spriteSheet[child.GetSiblingIndex()] = child.gameObject; }
-        //ottiene la velocità iniziale dello slime
-        startSpeed = speed;
 
+        //ottiene la velocità iniziale dello slime
+        //startSpeed = speed;
+
+        //fa partire la coroutine per l'inseguimento del giocatore
         StartCoroutine(Follow());
 
     }
@@ -43,47 +45,60 @@ public class CubicSlimeBehaviour : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        //se bisogna seguire il giocatore...
         if (followPlayer)
         {
-
+            //...il cubo slime si muove verso il giocatore
             transform.position = Vector3.Lerp(transform.position, player.position, speed * Time.fixedDeltaTime);
 
         }
         
     }
-
+    /// <summary>
+    /// Si occupa dei tempismi di inseguimento e attacco
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Follow()
     {
-
+        //segue il giocatore per un po'
         yield return new WaitForSeconds(followTime);
-
+        //smette di seguirlo
         followPlayer = false;
 
+        //yield return new WaitForSeconds(attackCD);
+
+        //inizia la coroutine d'attacco
+        StartCoroutine(Attack(1));
+        //aspetta un po'
         yield return new WaitForSeconds(attackCD);
-
-        StartCoroutine(Attack());
-
-        yield return new WaitForSeconds(attackCD);
-
+        //torna a seguire il giocatore
         followPlayer = true;
+        //riporta lo slime allo sprite iniziale
+        //spriteSheet[actualAnimState - 1].SetActive(false);
+        //spriteSheet[0].SetActive(true);
+        StartCoroutine(Attack(-1));
+
+        //fa ripartire la coroutine
+        StartCoroutine(Follow());
 
     }
 
-    private IEnumerator Attack()
+    private IEnumerator Attack(int animationSpeed)
     {
 
-        actualAnimState++;
+        actualAnimState += animationSpeed;
 
         yield return new WaitForSeconds(attackSpeed);
 
-        if (actualAnimState == spriteSheet.Length)
+        if (!(actualAnimState <= 0) && !(actualAnimState >= spriteSheet.Length))
         {
             
-            spriteSheet[actualAnimState - 1].SetActive(false);
+            spriteSheet[actualAnimState - animationSpeed].SetActive(false);
 
             spriteSheet[actualAnimState].SetActive(true);
-        
+
+            StartCoroutine(Attack(animationSpeed));
+
         }
 
     }
